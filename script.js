e70ec8271dfc237c9eae1a77e56b6ab03d78e69b
isackpate <isackpate@gmail.com>
2025-05-14 15:14:15 +0300

    Add files via upload
    Added website with new features

---
+++ script.js
@@ -0,0 +1,8 @@
+function switchLanguage() {
+    alert("Language switcher itakuja hivi karibuni...");
+}
+
+document.getElementById('productForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Bidhaa imeongezwa (mfano)!');
+});
